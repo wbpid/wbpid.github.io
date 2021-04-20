6304d53e2527584b7aa009e6c6d1920127f29f18
Web Belajar Pemrograman <82552579+wbpid@users.noreply.github.com>
2021-04-20 17:58:30 +0800

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,18 +1,14 @@
-RUN.titleNav = s =>{
-  const t = $('#title-nav');
-  if(s){
-    let x1, x2, x3, n = '';
-    $('h1 div').each(function(){
-      x1 = $('#body').scrollTop();
-      x2 = $(this).offset().top;
-      x3 = (window.innerWidth > 600)? 150 : 80;
-      n += '<button onclick="RUN.jumpTo('+ Math.round(x1 + x2 - x3) +'); RUN.titleNav(false)" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
-    });
-    t.show().find('nav').html(n);
-  }
-  else t.hide();
-};
-
-(()=>{
-  if($('h1 div').length == 0) $('#title-nav, .title-nav').remove();
-})();
+window.addEventListener('load', ()=>{
+  setTimeout(()=>{
+    if($('h1 div').length > 0){
+      let x1, x2, n = '';
+      $('h1 div').each(function(){
+        x1 = $(this).offset().top;
+        x2 = (window.innerWidth > 600)? 150 : 80;
+        n += '<button onclick="RUN.jumpTo('+ Math.round(x1 - x2) +'); $(\'#title-nav\').hide()" class="w3-bar-item w3-button">'+ $(this).text() +'</button>';
+      });
+      $('#title-nav nav').html(n);
+    }
+    else $('#title-nav, .title-nav').remove();
+  }, 100);
+});
