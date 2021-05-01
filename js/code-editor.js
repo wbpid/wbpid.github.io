daad72843a44d0055b2c85b38c92eab6462dd6e8
Web Belajar Pemrograman <82552579+wbpid@users.noreply.github.com>
2021-05-01 16:44:10 +0800

    Update code-editor.js

--- js/code-editor.js
+++ js/code-editor.js
@@ -1,9 +1,13 @@
 const codeEditor = ()=>{
   const t = $('#x-editor textarea');
-  $('#x-frame').attr('srcdoc', ('<!DOCTYPE html><html><head><base href="'+ ENV.repoUrl +'/"/><style>'+ t.eq(1).val() +'</style></head><body>'+ t.eq(0).val() +'<script>'+ t.eq(2).val() +'</script></body></html>'));
+  const w = window.open('', 'x-frame').document;
+  w.open();
+  w.write('<!DOCTYPE html><html><head><base href="'+ ENV.repoUrl +'/"/><style>'+ t.eq(1).val() +'</style></head><body>'+ t.eq(0).val() +'<script>'+ t.eq(2).val() +'</script></body></html>');
+  w.close();
 };
 
-(()=>{
+window.addEventListener('load', ()=>{
+  setTimeout(codeEditor, 100);
   const b = $('.x-btn');
   const t = $('#x-editor textarea');
   b.click(function(){
@@ -13,4 +17,4 @@ const codeEditor = ()=>{
     t.hide();
     t.eq(i).show();
   });
-})();
+});
