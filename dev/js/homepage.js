cad1294a4855af3b064d8ae9248bd51331ef18cd
Web Belajar Pemrograman <82552579+wbpid@users.noreply.github.com>
2021-05-04 00:24:36 +0800

    Update homepage.js

--- dev/js/homepage.js
+++ dev/js/homepage.js
@@ -16,35 +16,37 @@ const SLIDE = {
   }
 };
 
-$(document).ready(()=>{
-  $('.slide').show();
-  const a = $('.a-slide');
-  $('#body').scroll(function(){
-    if(a.length > 0){
-      const h = $(this).height();
-      const s = $(this).scrollTop();
-      a.each(function(){
-        const t = $(this).offset().top;
-        if(((h+s)-(s+t)) > 50){
-          $(this).find('.a-left').addClass('w3-animate-left');
-          $(this).find('.a-right').addClass('w3-animate-right');
-          $(this).removeClass('a-slide');
-        }
-      });
-    }
-  });
-  const c = $('.code');
-  c.eq(0).find('code').text('\n function tambah(a, b){\n   return a + b;\n }\n function kurang(a, b){\n   return a - b;\n }\n\n console.log(tambah(1, 2));\n console.log(kurang(9, 4));\n ');
-  c.eq(1).find('code').text('\n body {\n   background: blue;\n }\n h1 {\n   text-align: center;\n }\n p {\n   font-size: 20px;\n }\n ');
-  c.eq(2).find('code').text('\n <html>\n   <head>\n     <title>Nama web</title>\n   </head>\n   <body>\n     <h1>Judul artikel</h1>\n     <p>Paragraf</p>\n   </body>\n </html>\n ');
-  c.click(()=>{
-    const r = e =>{
-      if($(e).hasClass('code1')) $(e).removeClass('code1').addClass('code2');
-      else if($(e).hasClass('code2')) $(e).removeClass('code2').addClass('code3');
-      else $(e).removeClass('code3').addClass('code1');
-    };
-    r('.code.html');
-    r('.code.css');
-    r('.code.javascript');
-  });
+window.addEventListener('load', ()=>{
+  setTimeout(()=>{
+    $('.slide').show();
+    const a = $('.a-slide');
+    $('#body').scroll(function(){
+      if(a.length > 0){
+        const h = $(this).height();
+        const s = $(this).scrollTop();
+        a.each(function(){
+          const t = $(this).offset().top;
+          if(((h+s)-(s+t)) > 50){
+            $(this).find('.a-left').addClass('w3-animate-left');
+            $(this).find('.a-right').addClass('w3-animate-right');
+            $(this).removeClass('a-slide');
+          }
+        });
+      }
+    });
+    const c = $('.code');
+    c.eq(0).find('code').text('\n function tambah(a, b){\n   return a + b;\n }\n function kurang(a, b){\n   return a - b;\n }\n\n console.log(tambah(1, 2));\n console.log(kurang(9, 4));\n ');
+    c.eq(1).find('code').text('\n body {\n   background: blue;\n }\n h1 {\n   text-align: center;\n }\n p {\n   font-size: 20px;\n }\n ');
+    c.eq(2).find('code').text('\n <html>\n   <head>\n     <title>Nama web</title>\n   </head>\n   <body>\n     <h1>Judul artikel</h1>\n     <p>Paragraf</p>\n   </body>\n </html>\n ');
+    c.click(()=>{
+      const r = e =>{
+        if($(e).hasClass('code1')) $(e).removeClass('code1').addClass('code2');
+        else if($(e).hasClass('code2')) $(e).removeClass('code2').addClass('code3');
+        else $(e).removeClass('code3').addClass('code1');
+      };
+      r('.code.html');
+      r('.code.css');
+      r('.code.javascript');
+    });
+  }, 8);
 });
