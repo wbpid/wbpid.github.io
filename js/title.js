51d0316db8fd54f8480e192f6f85a9c6342c57c2
Web Belajar Pemrograman <82552579+wbpid@users.noreply.github.com>
2021-05-06 16:41:48 +0800

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,10 +1,11 @@
 const TITLE = {
   open: ()=>{
-    let x1, x2, n = '';
+    let x1, x2, x3, n = '';
     $('h1 div').each(function(){
-      x1 = $(this).offset().top;
-      x2 = (window.innerWidth > 600)? 150 : 80;
-      n += '<button onclick="TITLE.scroll('+ Math.round(x1 - x2) +')" class="w3-bar-item w3-hover-light-gray w3-button">'+ $(this).text() +'</button>';
+      x1 = $('#body').scrollTop();
+      x2 = $(this).offset().top;
+      x3 = (window.innerWidth > 600)? 150 : 80;
+      n += '<button onclick="TITLE.scroll('+ Math.round(x1 - (x2 - x3)) +')" class="w3-bar-item w3-hover-light-gray w3-button">'+ $(this).text() +'</button>';
     });
     $('#title-nav').show().find('nav').html(n);
   },
